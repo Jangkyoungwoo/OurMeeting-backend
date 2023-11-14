7a66df3a50a2fea5de65ec1e5b101d26d231af8c
장경우 <jangkyoungwoo@jang-gyeong-uui-MacBookAir.local>
2023-11-14 20:23:24 +0900

    3

--- src/main/java/com/example/sdpBackEnd/SdpBackEndApplication.java
+++ src/main/java/com/example/sdpBackEnd/SdpBackEndApplication.java
@@ -15,15 +15,15 @@ public class SdpBackEndApplication {
 		SpringApplication.run(SdpBackEndApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000")
-						.allowCredentials(true);
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+//				registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+//						.allowCredentials(true);
+//			}
+//		};
+//	}
 }
